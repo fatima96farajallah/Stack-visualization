a8e9adece59e8a9a03dddc45157c40f057504c6c
fatima96farajallah <farajallahfatima@gmail.com>
2019-10-16 18:11:39 +0300

    edit scss code

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function push() {
+    let elem = document.getElementById("div1");   
+    let pos = 0;
+    let id = setInterval(frame, 5);
+    function frame() {
+      if (pos == 350) {
+        clearInterval(id);
+      } else {
+        pos++; 
+        elem.style.top = pos + "px"; 
+        elem.style.left = pos + "px"; 
+      }
+    }
+  }
